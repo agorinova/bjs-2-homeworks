41b466f23bcbbce52e572af9af5be88da022049e
Gorinova Nastya <shepetova89@mail.ru>
2023-02-02 12:03:46 +0300

    Первая сдача второго задания

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,22 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] > min) {
+      return max === arr[i];
+    } else (arr[i] < max) {
+      return min === arr[i];
+    }
+    sum += arr[i]
+  }
 
   return { min: min, max: max, avg: avg };
+
+  let averageValue = sum / arr.length;
+
+  return Number(averageValue.toFixed(2));
 }
 
 function summElementsWorker(...arr) {
