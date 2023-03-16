3fcf3d442c9aab3048df83b2e09c5f1d52ffd15c
Gorinova Nastya <shepetova89@mail.ru>
2023-03-16 15:48:00 +0300

    Классы 16.03.2023

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -6,10 +6,63 @@ class PrintEditionItem {
         this.state = state;
         this.type = type;
     }
+
+    fix(){
+        let improvement = this.state * 1.5;
+        return improvement;
+    }
+    
+    set state(bookState) {
+        if(bookState < 0) {
+            this.state = 0;
+        } else if (bookState > 100) {
+            this.state = 100;
+        } else {
+            this._state = bookState;
+        }
+    }
+    
+    get state() {
+        return this._state;
+    }
 }
 
-let defective = new PrintEditionItem;
-
-defective.fix = function(){
-    return state*1.5;
+class Magazine extends PrintEditionItem {
+    constructor (name, releaseDate, pagesCount, state = 100, type = null){
+        super(name, releaseDate, pagesCount, state = 100, type = null);
+        this.type = "magazine";
+    }
 }
+
+class Book extends PrintEditionItem {
+    constructor (name, releaseDate, pagesCount, state = 100, type = null, author){
+        super(name, releaseDate, pagesCount, state = 100, type = null);
+        this.type = "book";
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author){
+        super(author);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author){
+        super(author);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author){
+        super(author);
+        this.type = "detective";
+    }
+}
+
+let printEditionItem = new PrintEditionItem();
+
+
+
