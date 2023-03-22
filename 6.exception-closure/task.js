5f86a857442e99f9604d6cbee0acccc73fb7d3c7
Gorinova Nastya <shepetova89@mail.ru>
2023-03-22 14:14:00 +0300

    Сдача. Обработка исключений и замыканий. 22.03.2023

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount(a){
+    let parseResult = Number.parseFloat(a);
+    if(Number.isNaN(parseFloat)){
+        throw new Error("Невалидное значение");
+    }
+    return parseResult;
+}
+
+function validateCount(a){
+    try {
+        return parseCount(a);
+    } catch(error){
+        return error;
+    }
+}
+
+
+
+class Triangle {
+    constructor (a, b, c){
+        if((a + b) < c || (a + c) < b || (b + c) < a){
+            throw new Error("Треугольник с такими сторонами не существует"); 
+        } else {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+
+    get area(){
+        let p = 0.5 * (this.perimeter);
+        return (Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c){
+    try {
+        return new Triangle(a, b, c); 
+    } catch (error) {
+        return {
+            get area(){
+                return "Ошибка! Треугольник не существует";
+            },
+
+            get perimeter(){
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
